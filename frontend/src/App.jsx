3eb3cc48a421477999bd1bd952b7d79fe7ac17a6
Rahul Saini <111075975+Rahul-K-Saini@users.noreply.github.com>
2024-02-03 18:15:51 +0530

    Update App.jsx
    updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import LandingPage from './Components/LandingPage.jsx'
 import About from './Components/About.jsx'
 
 
 
-const router = createBrowserRouter([
-    { path: '/', element: <LandingPage /> },
-    { path: '/about', element: <About /> }
-])
+// const router = createBrowserRouter([
+//     { path: '/', element: <LandingPage /> },
+//     { path: '/about', element: <About /> }
+// ])
 
 
 const App = () => {
     return (
         <>
-            <RouterProvider router={router} />
+{/*             <RouterProvider router={router} /> */}
+        <LandingPage/>
         </>
     )
 }
