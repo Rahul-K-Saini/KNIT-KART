e570f1a95091f49c56247819a3738299c04afca5
Arshil <arshilamaanansari@gmail.com>
2024-02-03 16:29:09 +0530

    app added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LandingPage from './Components/LandingPage.jsx'
+import About from './Components/About.jsx'
+
+
+
+const router = createBrowserRouter([
+    { path: '/', element: <LandingPage /> },
+    { path: '/about', element: <About /> }
+])
+
+
+const App = () => {
+    return (
+        <>
+            <RouterProvider router={router} />
+            {/* <h1>Hello</h1> */}
+        </>
+    )
+}
+
+export default App
