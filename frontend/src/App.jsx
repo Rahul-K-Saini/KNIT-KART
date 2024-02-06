f2c1ba8af68c5abea860dc00de2ec9709fa63a71
RahulKSaini <rahul545436@gmail.com>
2024-02-07 01:36:12 +0530

    initial commit theme context and toggle button added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { ThemeProvider } from "./context/ThemeProvider";
+import Navbar from "./components/Navbar/Navbar";
+
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Navbar />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import  ThemeToggle  from './theme-toggle';
+
+function Navbar() {
+  return (
+    <>
+        <h2>KNIT-<span className='font-semibold'>KART</span></h2>
+        
+        <ThemeToggle />
+    </>
+  )
+}
+
+export default Navbar
