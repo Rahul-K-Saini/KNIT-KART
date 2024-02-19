9ae46cc6c889442156b50df670ba3e3cd889bca7
RahulKSaini <rahul545436@gmail.com>
2024-02-19 23:39:26 +0530

    added post ad page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import Entrance from "./Pages/Entrance/Entrance";
 import AdPage from "./Pages/AdPage/AdPage";
+import PostAd from "./Pages/PostAd";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
       {
         path: "ad",
         element: <AdPage />,
+      },
+      {
+        path: "postad",
+        element: <PostAd />,
       },
     ],
   },
