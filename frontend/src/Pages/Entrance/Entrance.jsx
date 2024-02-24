85155b8f918d440b3b0ab404ca595e7e2cc10493
Arshil <arshilamaanansari@gmail.com>
2024-02-24 16:40:46 +0530

    starting to work

--- frontend/src/Pages/Entrance/Entrance.jsx
+++ frontend/src/Pages/Entrance/Entrance.jsx
@@ -55,72 +55,85 @@ const Entrance = () => {
                 />
             });
         }
+        console.log(isLogin.login);
     };
 
-    
+    // {/* <div className={`bg-white  absolute top-0 w-full h-full z-10 ${!isLogin.login ? style['login-slide'] : style['signup-slide']}`}>
+    //                         {/* Slider Window */}
+    //                         <div className={style['slider-window']}>
+    //                                 {isLogin.compon}
+    //                         </div>
+    //                     </div> */}
 
     return (
-        <div className='w-full px-12 py-16'>
-            <div className='bg-white rounded flex w-full h-full'>
-                {/* Log In Section */}
-                <div className='flex flex-col justify-center relative items-center space-y-3 w-1/2'>
-                    <div className={`bg-white  absolute top-0 w-full h-full z-10 ${!isLogin.login ? style['login-slide'] : style['signup-slide']}`}>
-                        {/* Slider Window */}
-                        <div className={style['slider-window']}>
-                                {isLogin.compon}
+        <div className='flex items-center justify-center h-96'>
+
+            <div className={style["flip-card"]}>
+                <div className={`${style["flip-card-inner"]} ${!isLogin.login ? 'flipping' : ''}`}>
+
+                    <div className={style["flip-card-front"]}>
+
+                        {/* Log In Section */}
+                        <div className='w-96 relative items-center space-y-3'>
+                            <form className={style.form}>
+                                <p className={style.title}>Log In </p>
+                                <label>
+                                    <input className={style.input} type="email" placeholder="" required="" />
+                                    <span>Email</span>
+                                </label>
+
+                                <label>
+                                    <input className={style.input} type="password" placeholder="" required="" />
+                                    <span>Password</span>
+                                </label>                        <button className={style.submit}>Submit</button>
+                                <p className={style.signin}>Don't have an account yet?   <Link onClick={handleToggle}>Signup here.</Link> </p>
+                            </form>
+                        </div>
+
+                    </div>
+
+
+                    <div className={style["flip-card-back"]}>
+
+
+                        {/* Sign Up Section */}
+                        <div className='w-96 space-y-3 rounded-3xl'>
+                            <form className={style.form}>
+                                <p className={style.title}>Register </p>
+                                <p className={style.message}>Signup now and get full access to our app. </p>
+                                <div className={style.flex}>
+                                    <label>
+                                        <input className={style.input} type="text" placeholder="" required="" />
+                                        <span>Firstname</span>
+                                    </label>
+
+                                    <label>
+                                        <input className={style.input} type="text" placeholder="" required="" />
+                                        <span>Lastname</span>
+                                    </label>
+                                </div>
+
+                                <label>
+                                    <input className={style.input} type="email" placeholder="" required="" />
+                                    <span>Email</span>
+                                </label>
+
+                                <label>
+                                    <input className={style.input} type="password" placeholder="" required="" />
+                                    <span>Password</span>
+                                </label>
+                                <label>
+                                    <input className={style.input} type="password" placeholder="" required="" />
+                                    <span>Confirm password</span>
+                                </label>
+                                <button className={style.submit}>Submit</button>
+                                <p className={style.signin}>Already have an account ?   <Link onClick={handleToggle}>Signin here.</Link> </p>
+                            </form>
                         </div>
                     </div>
-                    <form className={style.form}>
-                        <p className={style.title}>Log In </p>
-                        <label>
-                            <input className={style.input} type="email" placeholder="" required="" />
-                            <span>Email</span>
-                        </label>
-
-                        <label>
-                            <input className={style.input} type="password" placeholder="" required="" />
-                        <span>Password</span>
-                        </label>                        <button className={style.submit}>Submit</button>
-                        <p className={style.signin}>Don't have an account yet?   <Link onClick={handleToggle}>Signup here.</Link> </p>
-                    </form>
                 </div>
-
-                {/* Sign Up Section */}
-                <div className=' flex flex-col justify-center items-center space-y-3 w-1/2 rounded-3xl'>
-                    <form className={style.form}>
-                        <p className={style.title}>Register </p>
-                        <p className={style.message}>Signup now and get full access to our app. </p>
-                        <div className={style.flex}>
-                            <label>
-                                <input className={style.input} type="text" placeholder="" required="" />
-                                <span>Firstname</span>
-                            </label>
-
-                            <label>
-                                <input className={style.input} type="text" placeholder="" required="" />
-                                <span>Lastname</span>
-                            </label>
-                        </div>
-
-                        <label>
-                            <input className={style.input} type="email" placeholder="" required="" />
-                            <span>Email</span>
-                        </label>
-
-                        <label>
-                            <input className={style.input} type="password" placeholder="" required="" />
-                            <span>Password</span>
-                        </label>
-                        <label>
-                            <input className={style.input} type="password" placeholder="" required="" />
-                            <span>Confirm password</span>
-                        </label>
-                        <button className={style.submit}>Submit</button>
-                        <p className={style.signin}>Already have an account ?   <Link onClick={handleToggle}>Signin here.</Link> </p>
-                    </form>
-                </div>
-
             </div>
+
         </div>
     );
 }
