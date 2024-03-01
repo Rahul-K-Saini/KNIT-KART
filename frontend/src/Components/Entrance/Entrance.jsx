7e00914faa2efeecbf7be4df74576c27aa88ad67
rakeshkumar22743 <ry8319100@gmail.com>
2024-03-02 01:18:31 +0530

    fix: entrance responsive fix

--- frontend/src/Components/Entrance/Entrance.jsx
+++ frontend/src/Components/Entrance/Entrance.jsx
@@ -1,89 +1,156 @@
-import React, { useState } from 'react';
-import styles from './entrance.module.css';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import * as Components from './Components';
+//  import { ResponsiveContainer } from './ResponsiveComponents';
+import "./styles.css";
+import style from "./Entrance.module.css";
 
+function App() {
+    const [signIn, toggle] = React.useState(true);
 
-const Entrance = () => {
-    const [isLogin, setIsLogin] = useState({
-        login: false,
-        headingText: "Already Registered",
-        buttonText: "Log In here"
-    });
+    const [isLoginPage, setIsLoginPage] = useState({ loginPage: true, class: '' });
 
-    const handleToggle = () => {
-        if (isLogin.login) {
-            setIsLogin({
-                login: false,
-                headingText: "Already Registered",
-                buttonText: "Log In here"
-            });
+    const loginToggle = () => {
+        if (isLoginPage.loginPage) {
+            setIsLoginPage({ loginPage: false, class: 'animate-signUp' });
         } else {
-            setIsLogin({
-                login: true,
-                headingText: "Not Registered yet",
-                buttonText: "Signup here"
-            });
+            setIsLoginPage({ loginPage: true, class: 'animate-signIn' });
         }
-    };
+    }
 
     return (
-        <div classNameName='bg-cyan-200 w-full h-screen px-12 py-14'>
-            <div classNameName=' rounded flex w-full h-full'>
-                {/* Log In Section */}
-                <div classNameName='flex flex-col justify-center relative items-center space-y-3 bg-slate-300 w-1/2 rounded-3xl'>
-                    {/* Slider Window */}
-                    <div classNameName={`bg-pink-500 absolute top-0 w-full h-full z-10 rounded-3xl ${!isLogin.login ? styles['login-slide'] : styles['signup-slide']}`}>
-                        <div classNameName={styles['slider-window']}>
-                            <h1>{isLogin.headingText}</h1>
-                            <Link to={`?mode=${isLogin.login ? 'signup' : 'login'}`} onClick={handleToggle}>{isLogin.buttonText}</Link>
-                        </div>
-                    </div>
-                    <h1 classNameName='text-4xl font-bold text-zinc-800'>Log In</h1>
-                    <div classNameName='flex flex-col justify-center items-start space-y-8 py-7'>
-                        <input type="email" id="email" placeholder='Enter Your Registered Email' classNameName='drop-shadow-xl text-xl p-2 rounded font-light' />
-                        <input type="password" id="password" placeholder='Enter Your Password' classNameName='drop-shadow-xl text-xl p-2 rounded font-light' />
-                        <button classNameName='bg-blue-600 text-white px-3 py-2 rounded-md text-xl drop-shadow-xl'>Log In</button>
-                        <a href="" classNameName='text-blue-600 underline'>Forgot Password?</a>
-                    </div>
-                </div>
-
-                {/* Sign Up Section */}
+        <>
+            <div className={style["desktop-form"]}>
                 
-                <form className="form">
-    <p className="title">Register </p>
-    <p className="message">Signup now and get full access to our app. </p>
-        <div className="flex">
-        <label>
-            <input className="input " type="text" placeholder="" required=""/>
-            <span>Firstname</span>
-        </label>
+                {/* <ResponsiveContainer> */}
+                <Components.Container>
+                    <Components.SignUpContainer signinIn={signIn}>
+                        <Components.Form>
+                            <Components.Title>Create Account</Components.Title>
+                            <Components.Input type='text' placeholder='Email(KNIT)' />
+                            <Components.Input type='email' placeholder='Contact No.' />
+                            <Components.Input type='password' placeholder='Password' />
+                            <Components.Input type='password' placeholder='Confirm Password' />
+                            {/* <Components.Input type='password' placeholder='Password' /> */}
+                            {/* <Components.Input type='password' placeholder='Confirm Password' />
+                      <Components.Input type='password' placeholder='Rigster Now' /> */}
+                            <Components.Button>Sign Up</Components.Button>
+                        </Components.Form>
+                    </Components.SignUpContainer>
 
-        <label>
-            <input className="input" type="text" placeholder="" required=""/>
-            <span>Lastname</span>
-        </label>
-    </div>  
-            
-    <label>
-        <input className="input" type="email" placeholder="" required=""/>
-        <span>Email</span>
-    </label> 
-        
-    <label>
-        <input className="input" type="password" placeholder="" required=""/>
-        <span>Password</span>
-    </label>
-    <label>
-        <input className="input" type="password" placeholder="" required=""/>
-        <span>Confirm password</span>
-    </label>
-    <button className="submit">Submit</button>
-    <p className="signin">Already have an acount ? <a href="#">Signin</a> </p>
-</form>
 
+                    <Components.SignInContainer signinIn={signIn}>
+                        <Components.Form>
+                            <Components.Title>Sign in</Components.Title>
+                            <Components.Input type='email' placeholder='Email' />
+                            <Components.Input type='password' placeholder='Password' />
+                            <Components.Anchor href=''>Forgot your password?</Components.Anchor>
+                            <Components.Button>Sigin In</Components.Button>
+                        </Components.Form>
+                    </Components.SignInContainer>
+
+
+                    <Components.OverlayContainer signinIn={signIn}>
+                        <Components.Overlay signinIn={signIn}>
+
+                            <Components.LeftOverlayPanel signinIn={signIn}>
+                                <Components.Title>Welcome Back!</Components.Title>
+                                <Components.Paragraph>
+                                    To keep connected with us please login with your personal info
+                                </Components.Paragraph>
+                                <Components.GhostButton onClick={() => toggle(true)}>
+                                    Sign In
+                                </Components.GhostButton>
+                            </Components.LeftOverlayPanel>
+
+                            <Components.RightOverlayPanel signinIn={signIn}>
+                                <Components.Title>Hello, Friend!</Components.Title>
+                                <Components.Paragraph>
+                                    Enter Your personal details and start journey with us
+                                </Components.Paragraph>
+                                <Components.GhostButton onClick={() => toggle(false)}>
+                                    Sigin Up
+                                </Components.GhostButton>
+                            </Components.RightOverlayPanel>
+
+                        </Components.Overlay>
+                    </Components.OverlayContainer>
+
+                </Components.Container>
+                {/* </ResponsiveContainer> */}
             </div>
-        </div>
-    );
+
+
+
+            <div className={style["mobile-form"]}>
+                <div className={`wrapper ${isLoginPage.class}`}>
+                    <div className="form-wrapper sign-Up">
+                        <form action="">
+                            <h2>Sign Up</h2>
+                            <div className={style["input-group"]}>
+                                <input type="text" required />
+                                <label for="">Email(KNIT)</label>
+                                <i className='bx bxs-user'></i>
+                            </div>
+                            <div className={style["input-group"]}>
+                                <input type="email" required />
+                                <label for="">Contact No.</label>
+                                <i className='bx bxs-envelope'></i>
+                                <i className='bx bxs-user'></i>
+                            </div>
+
+                            <div className={style["input-group"]}>
+                                <input type="password" required />
+                                <label for="">Password</label>
+                                <i className='bx bxs-lock-alt'></i>
+                            </div>
+
+                            <div className={style["input-group"]}>
+                                <input type="password" required />
+                                <label for="">Confirm Password</label>
+                                <i className='bx bxs-lock-alt'></i>
+                            </div>
+
+                            {/* <div className={style["remember"]}>
+                                <label><input type="checkbox"/> I agree to the terms & conditions</label>
+                            </div> */}
+                            <button type="submit" className={style["btn"]}>Sign Up</button>
+
+                            <div className={style["Sign-link"]}>
+                                <p>Already have an account? <a href="#" onClick={loginToggle} className={style["SignIn-link"]}>Sign In</a></p>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div className="form-wrapper sign-in">
+                        <form action="">
+                            <h2>Login</h2>
+                            <div className={style["input-group"]}>
+                                <input type="text" required />
+                                <label for="">Email</label>
+                                <i className='bx bxs-user'></i>
+                            </div>
+
+                            <div className={style["input-group"]}>
+                                <input type="password" required />
+                                <label for="">Password</label>
+                                <i className='bx bxs-lock-alt'></i>
+                            </div>
+                            <div className={style["forgot-password"]}>
+                                <a href="">Forgot Password?</a>
+                            </div>
+                            <button type="submit" className={style["btn"]}>Login</button>
+                            <div className={style["Sign-link"]}>
+                                <p>Don't have an account? <a href="#" onClick={loginToggle} className={style["SignUp-link"]}>Sign Up</a></p>
+                            </div>
+
+                        </form>
+                    </div>
+
+
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Entrance;
+export default App;
