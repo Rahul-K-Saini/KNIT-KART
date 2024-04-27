e438bb72a22d226830429a384336bfd84332d300
Shantanu <shantanusaini7000@gmail.com>
2024-04-28 00:03:09 +0530

    fix: navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,8 @@ import { Search } from "lucide-react";
 import logo from "../../assets/images/logo.png";
 import logoDark from "../../assets/images/logo-dark.png";
 import { useSelector } from "react-redux";
+import { FaRegUserCircle } from "react-icons/fa";
+import { HiOutlineBuildingStorefront } from "react-icons/hi2";
 
 function Navbar() {
   const theme = useSelector((state) => state.theme.theme);
@@ -21,38 +23,48 @@ function Navbar() {
   return (
     <nav className="w-full px-4 py-1 top-0 sticky z-50 border-b-2 border-gray-300  bg-background">
       <div className="flex justify-between items-center">
-        <div>
+        <div className="min-w-fit">
           <Link to="/">
             <img
               src={theme === "light" ? logo : logoDark}
               alt="logo"
-              className="w-25 h-20"
+              className="w-auto h-20"
             />
           </Link>
         </div>
-        <div className="flex items-center space-x-8">
-          <div
-            className={`relative ${
-              isFocused ? "w-60" : "w-40"
-            } transition-width duration-300 ease-in-out`}
-          >
-            <input
-              type="search"
-              placeholder="Search"
-              className="rounded-full px-4 py-2 w-full"
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-            />
-            <Search className="absolute right-3 top-1/2 transform -translate-y-1/2" />
+
+        <div className="flex items-center md:space-x-8 space-x-3">
+
+          <div>
+            <div className={`relative ${isFocused ? "w-60" : "w-40"
+              } transition-width duration-300 ease-in-out md:block hidden`}>
+                <input
+                type="search"
+                placeholder="Search"
+                className="rounded-full px-4 py-2 w-full"
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+              />
+              <Search className="absolute right-3 top-1/2 transform -translate-y-1/2" />
+            </div>
+
+            <div className="relative md:hidden block">
+              <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-text" />
+            </div>
           </div>
-          <button className="dark:text-gray-50 bg-secondary data:bg-secondary px-4 py-2 rounded hover:opacity-85 transform transition-all duration-300 hover:scale-105">
-            <a href="#products">Products</a>
+
+          <button className="flex item-center justify-center dark:text-gray-50 bg-secondary data:bg-accent md:px-4 px-2 py-2 rounded hover:opacity-85 transform transition-all duration-300 hover:scale-105">
+            <a href="#products">
+              <span className="md:block mr-2 hidden">Products</span>
+            </a>
+            <HiOutlineBuildingStorefront className="text-xl" />
           </button>
           <Link
             to="/entrance"
-            className="dark:text-gray-50 bg-accent data:bg-accent px-4 py-2 rounded hover:opacity-85 transform transition-all duration-300 hover:scale-105"
+            className="flex item-center justify-center dark:text-gray-50 bg-accent data:bg-accent md:px-4 px-2 py-2 rounded hover:opacity-85 transform transition-all duration-300 hover:scale-105"
           >
-            Login
+            <span className="md:block mr-2 hidden">Login</span>
+            <FaRegUserCircle className="text-xl" />
           </Link>
           <ThemeToggle />
         </div>
