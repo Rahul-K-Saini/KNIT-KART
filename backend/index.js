f46f193582831594b12349627e134ee1e188af9e
RahulKSaini <rahul545436@gmail.com>
2024-05-08 20:37:55 +0530

    fixed cors policy error

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({origin:"http://localhost:5173", }));
 
 dotenv.config();
 
