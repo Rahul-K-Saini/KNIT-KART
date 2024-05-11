cd869f11fef165e4a3c6cf8fbf7dcd3c47578ae8
RahulKSaini <rahul545436@gmail.com>
2024-05-11 12:07:48 +0530

    fix major bug in profile section

--- frontend/src/Pages/Profile/Profile.jsx
+++ frontend/src/Pages/Profile/Profile.jsx
@@ -162,7 +162,7 @@ const Profile = () => {
                 {/* profile section start */}
                 {activeSection === 'profile' ? (
                     <div className="grid md:grid-cols-2 grid-cols-1 gap-4 md:w-9/12 w-10/12 mx-auto">
-                        {Object.entries(user?.user).map(([key, value]) => (
+                        {userDetails!= null ? Object.entries(user?.user).map(([key, value]) => (
                             <div key={key}>
                                 <label htmlFor={key} className="block mb-1">{key.charAt(0).toUpperCase() + key.slice(1)}</label>
                                 <input type="text" id={key} name={key} placeholder={key.charAt(0).toUpperCase() + key.slice(1)} className="border p-2 w-full focus:outline-none text-black" value={value} onChange={handleInputChange} disabled={disabled} />
