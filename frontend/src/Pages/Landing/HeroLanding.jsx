527743b49828c5417f7f2ec9f3eccacf33b594e7
Arshil <arshilamaanansari@gmail.com>
2024-05-11 13:16:24 +0530

    title fixed

--- frontend/src/Pages/Landing/HeroLanding.jsx
+++ frontend/src/Pages/Landing/HeroLanding.jsx
@@ -5,7 +5,7 @@ const HeroLanding = () => {
         <div style={{height:"calc( 100dvh - 60px)"}} className="relative h-screen">
             <div className="absolute inset-0 flex items-center justify-center">
                 <div className="text-center text-white p-4 md:p-8 w-full md:w-2/3">
-                    <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">MERE NO NAHI KATNE CHAIYE</h1>
+                    <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">KNIT KART</h1>
                     <h3 className='text-lg md:text-xl lg:text-2xl text-secondary'>KNIT's Arcade for Recycled Treasure</h3>
                     <p className="mt-14 md:mt-5 mb-14 md:text-lg text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sapiente sequi quibusdam. Amet itaque, fugiat, quidem, non architecto consequuntur recusandae ipsam laborum porro doloremque tempore earum ducimus quas nemo.</p>
                     <Link to='/home'  className="py-[10px] px-[15px] md:py-[10px]  md:px-[15px] bg-accent dark:bg-accent border-2 border-transparent rounded w-[40%] dark:text-[#f4f4f4] hover:border-black hover:dark:border-white duration-1000 transition-all">
