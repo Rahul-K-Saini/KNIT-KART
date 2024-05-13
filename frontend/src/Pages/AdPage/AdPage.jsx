39e92f6786e35b02807e7288c5c0e42e5e7912ba
Shantanu <shantanusaini7000@gmail.com>
2024-05-13 15:59:45 +0530

    fix: AdPage

--- frontend/src/Pages/AdPage/AdPage.jsx
+++ frontend/src/Pages/AdPage/AdPage.jsx
@@ -1,17 +1,8 @@
 import React, { useState, useEffect } from "react";
-import Image1 from "../../assets/images/product-1.jpg";
-import Image2 from "../../assets/images/product-2.jpg";
-import Image3 from "../../assets/images/product-3.jpg";
-import Image4 from "../../assets/images/product-4.jpg";
 import ClosePrev from "../../assets/images/icon-close.svg";
 import PrevBtn from "../../assets/images/icon-previous.svg";
 import NextBtn from "../../assets/images/icon-next.svg";
-import randImg from '../../assets/teamImages/rakesh.jpg'
 import "./AdPage.css";
-// import shortid from "shortid";
-// import { useDispatch } from "react-redux";
-// call the action I will use
-// import { add_items } from "../actions";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import axios from "axios";
