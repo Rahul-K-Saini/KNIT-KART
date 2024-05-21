9a3c9d54a14accf6ce107282959e1e8806d88cc8
Shantanu <shantanusaini7000@gmail.com>
2024-05-21 18:58:41 +0530

    feat: home page hero section

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carousel styles
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
 function Hero() {
